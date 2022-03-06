c884c5b6f75106cbc31e49c2ad0ecb4731a79f52
imhosseinzadeh <89960731+imanfirstgit@users.noreply.github.com>
2022-03-06 11:59:55 +0330

    remove additional method

--- bank-hibernate/src/main/java/ir/maktab/service/customerservice/AccountService.java
+++ bank-hibernate/src/main/java/ir/maktab/service/customerservice/AccountService.java
@@ -4,8 +4,6 @@ import ir.maktab.dao.customerdao.AccountDao;
 import ir.maktab.entity.customer.Account;
 import ir.maktab.service.AbstractCrudService;
 
-import java.util.Optional;
-
 public class AccountService extends AbstractCrudService<Account, String> {
 
     public AccountService() {
@@ -17,8 +15,4 @@ public class AccountService extends AbstractCrudService<Account, String> {
         return (AccountDao) super.getBaseDao();
     }
 
-    @Override
-    public Optional<Account> load(String s) {
-        return super.load(s);
-    }
 }
